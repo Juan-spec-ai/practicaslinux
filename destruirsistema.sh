ece86cb860adcfe04e05137d6c256c0781d28676
Juan-spec-ai <72024091+Juan-spec-ai@users.noreply.github.com>
2020-10-24 17:54:28 +0200

    Add files via upload

---
+++ destruirsistema.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+echo "DESTRUYENDO SISTEMA..."
+echo -n "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"
+
+tput cub 17
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
+
+echo -n "█"
+sleep $(($RANDOM%10))
